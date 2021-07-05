dc7ee852a515a72ae2be3836c9dbc92ff1611b90
Philipp Eller <peller.phys@gmail.com>
2021-07-05 20:12:44 +0200

    adding moran test

---
+++ spacings/moran.py
@@ -0,0 +1,62 @@
+import numpy as np
+from collections import namedtuple
+from scipy import stats
+from scipy.stats import distributions
+
+MorantestResult = namedtuple('MorantestResult', ('statistic', 'pvalue'))
+
+def moran_params(n):
+    mu = n * (np.log(n) + np.euler_gamma) - 0.5 - 1/(12*n)
+    var = n * (np.pi**2/6 - 1) - 0.5 - 1/(6*n)
+
+    c1 = mu - np.sqrt(var*n/2)
+    c2 = np.sqrt(var/(2*n))
+    
+    return c1, c2, mu, var
+
+def moran(x, cdf='uniform', args=()):
+    '''
+    Calculates the Moran test
+
+    Parameters:
+    -----------
+    x : array
+        1-D array of observations of random variables
+    cdf : string or callable
+        If a callable, that callable is used to calculate the cdf.
+        If a string, it should be the name of a distribution in `scipy.stats`,
+        which will be used as the cdf function.
+    args : tuple, sequence, optional
+        Distribution parameters for the `cdf`
+
+    Returns:
+    --------
+    statistic : float
+        Moran test statistic
+    pvalue :  float
+        p-value.
+
+    Examples:
+    ---------
+    ToDo
+
+    '''
+
+    if isinstance(cdf, str):
+        cdf = getattr(distributions, cdf).cdf
+
+    if np.ma.is_masked(x):
+        x = x.compressed()
+
+    N = len(x)
+
+    cdfvals = cdf(x, *args)
+    cdfvals = np.sort(cdfvals)
+    cdfvals = np.concatenate([[0], cdfvals, [1]])
+
+    ts = -np.sum(np.log(np.diff(cdfvals)))
+    c1, c2, mu, var = moran_params(N+1)
+    a = (ts - c1)/c2
+    p = 1 - stats.chi2(df=N+1).cdf(a)
+
+    return MorantestResult(ts, p)
